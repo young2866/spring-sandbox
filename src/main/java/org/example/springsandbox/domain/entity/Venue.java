f0f17c21b2403a7abe9dc7090a244eccc7e6fa73
young2866 <62212610+young2866@users.noreply.github.com>
2023-12-19 10:43:33 +0900

    [feature] 장소 엔티티

--- src/main/java/org/example/springsandbox/domain/entity/Venue.java
+++ src/main/java/org/example/springsandbox/domain/entity/Venue.java
@@ -5,29 +5,29 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Version;
 import java.time.LocalDateTime;
 import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
-public class User {
+public class Venue {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", nullable = false)
 	private Long id;
 
-	@Column(name = "email", nullable = false)
-	private String email;
+	@Column(name = "name", nullable = false)
+	private String name;
 
-	@Column(name = "nickname", nullable = false)
-	private String nickname;
-
-	@Column(name = "password", nullable = false)
-	private String password;
+	@Column(name = "address", nullable = false)
+	private String address;
 
 	@Column(name = "phone_number", nullable = false)
 	private String phoneNumber;
+
+	@Column(name = "category", nullable = false)
+	private String category;
+
 
 	@CreationTimestamp
 	@Column(name = "created_time")
@@ -36,6 +36,4 @@ public class User {
 	@CreationTimestamp
 	@Column(name = "modified_time")
 	private LocalDateTime modified_time = LocalDateTime.now();
-
-
 }
