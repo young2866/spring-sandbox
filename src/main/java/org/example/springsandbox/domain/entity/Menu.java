9e132dc307a289091dbe7d259e5cc40b248df731
young2866 <62212610+young2866@users.noreply.github.com>
2023-12-19 11:19:53 +0900

    [feature] 메뉴 엔티티

---
+++ src/main/java/org/example/springsandbox/domain/entity/Menu.java
@@ -0,0 +1,23 @@
+package org.example.springsandbox.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Menu {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Long id;
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Column(name = "price", nullable = false)
+	private Long price;
+
+}
